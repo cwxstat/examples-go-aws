472edd30a71e6d1a3ed528b1857fddd16d520a38
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-07-17 10:52:40 -0400

    update-automated-save

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # examples-go-aws
+
+## References:
+https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Query.html#Query.FilterExpression
--- main.go
+++ main.go
@@ -2,45 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/mchirico/go-aws/client"
-	db "github.com/mchirico/go-aws/dynamodb"
-
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/cwxstat/examples-go-aws/dynamo"
 )
 
-func Get(table string, pkey, skey string) (*db.PKSK, error) {
-
-	type KEY struct {
-		PK string `json:"PK"`
-		SK string `json:"SK"`
-	}
-
-	key, _ := attributevalue.MarshalMap(&KEY{
-		PK: pkey,
-		SK: skey,
-	})
-
-	input := &dynamodb.GetItemInput{
-		Key:             key,
-		TableName:       &table,
-		AttributesToGet: []string{"PK", "Doc", "SK", "Status"},
-	}
-	result, err := db.Get(client.Config(), input)
-	if err != nil {
-		return nil, err
-	}
-	p := &db.PKSK{}
-	err = attributevalue.UnmarshalMap(result.Item, p)
-	if err != nil {
-		return nil, err
-	}
-	return p, nil
-
-}
-
 func main() {
-	result, err := Get("mmcPKSK", "My Data", "Something")
+	result, err := dynamo.Get("mmcPKSK", "My Data", "Something")
 	if err != nil {
 		fmt.Println(err)
 	}
